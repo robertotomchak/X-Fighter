fd85180a503513308492194d8837d2c298f421e2
RobertoTomchak <robertotomchak@gmail.com>
2024-05-15 18:41:46 -0300

    Add Auxiliary Library

---
+++ auxiliary.c
@@ -0,0 +1,16 @@
+/* Defines multiple auxiliary functions, which many of the libraries will use
+*/
+
+// returns the maximum between two numbers
+long max(long a, long b)
+{
+    return a > b? a: b;
+}
+
+
+// returns the minimum between two numbers
+long min(long a, long b)
+{
+    return a < b? a: b;
+}
+
---
+++ auxiliary.h
@@ -0,0 +1,14 @@
+#ifndef AUXILIARY_H
+#define AUXILIARY_H
+
+/* Defines multiple auxiliary functions, which many of the libraries will use
+*/
+
+// returns the maximum between two numbers
+long max(long a, long b);
+
+
+// returns the minimum between two numbers
+long min(long a, long b);
+
+#endif
